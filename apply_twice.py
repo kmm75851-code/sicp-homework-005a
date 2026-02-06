f4f7d9685fa64f6b4c0dc60799ce09da317c9cfe
AbduLlah Levin <86154742+A-Levin@users.noreply.github.com>
2026-02-06 15:15:09 +0300

    Add apply_twice.py stub

---
+++ apply_twice.py
@@ -0,0 +1,11 @@
+def apply_twice(f, x):
+    """Применяет функцию f к x дважды.
+    
+    >>> apply_twice(lambda x: x + 1, 5)
+    7
+    >>> apply_twice(lambda x: x * 2, 3)
+    12
+    >>> apply_twice(lambda x: x * x, 2)
+    16
+    """
+    pass
