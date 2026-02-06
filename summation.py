feef67dee271f9a71224f9ddf16b99175edced9c
AbduLlah Levin <86154742+A-Levin@users.noreply.github.com>
2026-02-06 15:15:11 +0300

    Add summation.py stub

---
+++ summation.py
@@ -0,0 +1,11 @@
+def summation(n, term):
+    """Возвращает сумму term(1) + term(2) + ... + term(n).
+    
+    >>> summation(5, lambda x: x)
+    15
+    >>> summation(5, lambda x: x * x)
+    55
+    >>> summation(3, lambda x: x ** 3)
+    36
+    """
+    pass
